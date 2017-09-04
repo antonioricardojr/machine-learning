187ebb87ee41055e2442f65c98cf5fde25e9e63e
Antonio Marques Jr <antonioricardojr@gmail.com>
2017-09-04 19:10:26 -0300

    Defining step_gradient

--- demo.py
+++ demo.py
@@ -1,11 +1,31 @@
 
 from numpy import *
 
-def compute_error_for_given_point(b, m, points):
-	
+def step_gradient(b_current, m_current, points, learning_rate):
+	b_gradient = 0
+	m_gradient = 0
+	N = float(len(points))
 
-def step_gradient():
-	#gradient descendent
+	for i in range(0, len(points)):
+		x = points[i, 0]
+		y = points[i, 1]
+
+		b_gradient = -(2/N) * (y - ((m_current * x) + b_current))
+		m_gradient = -(2/N) * x * (y - ((m_current * x) + b_current))
+
+	new_b = b_current - (learning_rate * b_gradient)
+	new_m = m_current - (learning_rate * m_gradient)
+
+	return [new_b, new_m]
+
+def compute_error_for_given_point(b, m, points):
+	total_error = 0
+	for i in range(0,len(points)):
+		x = points[i, 0]
+		y = points[i, 1]
+		total_error += (y - (m*x + b)) **2
+
+	return total_error / float(len(points))
 
 def gradient_descendent_runner(points, starting_b, starting_m, learning_rate, num_iterations):
 	b = starting_b
