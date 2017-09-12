bebda05ef2f109f297c0b1ac4d5e1767a78518c1
Antonio Marques Jr <antonioricardojr@gmail.com>
2017-09-12 10:44:50 -0300

    Adding closed form

--- linear_regression/demo_mod.py
+++ linear_regression/demo_mod.py
@@ -7,13 +7,8 @@ if len(sys.argv) > 1:
 else:
 	data_file = 'income.csv'
 
-
-rss_file_output = open('./data/rss_output.csv', 'w')
-rss_file_output.write('iteration;rss\n') ##file header
-
 def write_rss(i,rss):
 	rss_file_output.write(str(i) + ';' + str(rss) + '\n')
-
 
 def step_gradient(b_current, m_current, points, learning_rate):
 	b_gradient = 0
@@ -46,21 +41,23 @@ def get_rss(b, m, points):
 		
 	return total_error 
 
-def gradient_descent_runner(points, starting_b, starting_m, learning_rate, num_iterations):
+def gradient_descent_runner(points, starting_b, starting_m, learning_rate, rss_tolerance):
 	b = starting_b
 	m = starting_m
 
-	for i in range(num_iterations):
+	rss = get_rss(b,m,points)
+
+	num_iterations = 0
+	while rss >= rss_tolerance: # mod here #
 		b, m = step_gradient(b, m, array(points), learning_rate)
 
 		rss = get_rss(b, m, points)
-		write_rss(i,rss)
-		print "RSS: {0}".format(rss)
+		print "iteration: {0} RSS: {1}".format(num_iterations, rss)
+		num_iterations+=1
 	return [b, m]
 
 def run():
 	points = genfromtxt(data_file, delimiter = ',')
-
 
 	learning_rate = input("Learning Rate: ")
 	#hyperparameters
@@ -71,15 +68,15 @@ def run():
 	initial_b = 0 #initial y-intercept guess
 	initial_m = 0 #initial slope guess
 
-	num_iterations = input("Number of Iterations: ")
-	if not int(num_iterations):
-		num_iterations = 1000
+	rss_tolerance = input("Error Tolerance: ")
+	if not int(rss_tolerance):
+		rss_tolerance = 895
 
-	print "Starting gradient descent at b = {0}, m = {1}, error = {2}".format(initial_b, initial_m, compute_error_for_line_given_points(initial_b, initial_m, points))
+	error = compute_error_for_line_given_points(initial_b, initial_m, points)
+	print "Starting gradient descent at b = {0}, m = {1}, error = {2}".format(initial_b, initial_m, error)
 	print "Running..."
-	[b, m] = gradient_descent_runner(points, initial_b, initial_m, learning_rate, num_iterations)
-	print "After {0} iterations b = {1}, m = {2}, error = {3}".format(num_iterations, b, m, compute_error_for_line_given_points(b, m, points))
-	rss_file_output.close()
+	[b, m] = gradient_descent_runner(points, initial_b, initial_m, learning_rate, rss_tolerance)
+	print "For {0} as rss tolerance, b = {1}, m = {2}, error = {3}".format(rss_tolerance, b, m, compute_error_for_line_given_points(b, m, points))
 
 if __name__ == '__main__':
     run()
